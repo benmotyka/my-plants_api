ccf2a447ab5c3c1a97e63ea0a0e12c21e235ad42
ben <benmotykax@gmail.com>
2022-09-07 20:45:24 +0200

    added sending verification code

--- src/modules/verificationCode/verificationCode.module.ts
+++ src/modules/verificationCode/verificationCode.module.ts
@@ -1,8 +1,27 @@
+import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { VerificationCodeController } from './verificationCode.controller';
 import { VerificationCodeService } from './verificationCode.service';
 
 @Module({
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: async (config: ConfigService) => ({
+        transport: {
+          host: config.get('SMTP_HOST'),
+          secure: false,
+          auth: {
+            user: config.get('SMTP_USER'),
+            pass: config.get('SMTP_PASSWORD'),
+          },
+        },
+        defaults: {
+          from: config.get('SMTP_FROM'),
+        },
+      }),
+    }),
+  ],
   controllers: [VerificationCodeController],
   providers: [VerificationCodeService],
   exports: [VerificationCodeService],
--- src/modules/verificationCode/verificationCode.service.ts
+++ src/modules/verificationCode/verificationCode.service.ts
@@ -1,19 +1,29 @@
 import { PrismaService } from '@modules/prisma/prisma.service';
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { SendEmailConfirmationRequestDto } from './dto/SendEmailConfirmation.request.dto';
 
 @Injectable()
 export class VerificationCodeService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private mailerService: MailerService,
+  ) {}
 
+  // @TODO: add language
   async sendEmailConfirmation(
     { email }: SendEmailConfirmationRequestDto,
     user: User,
   ) {
-    const code = '123';
-    console.log(email);
-    //send code
+    const code = this.generateCode();
+
+    await this.mailerService.sendMail({
+      to: email,
+      subject: 'Confirm your email',
+      html: `Verification code: ${code}`,
+    });
+
     await this.prisma.verificationCodes.create({
       data: {
         code,
@@ -21,5 +31,9 @@ export class VerificationCodeService {
         userId: user.id,
       },
     });
+  }
+
+  private generateCode() {
+    return `${Math.floor(10000 + Math.random() * 90000)}`;
   }
 }
