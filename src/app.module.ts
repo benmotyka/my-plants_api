bd5e905437334eca79753951979511c094880d5f
Ben <benmotykax@gmail.com>
2022-09-10 16:42:06 +0200

    disabled basicauth temporiarly

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,6 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { BasicAuth } from '@middleware/basic-auth';
 import { AuthModule } from '@modules/auth/auth.module';
 import { PlantModule } from '@modules/plant/plant.module';
 import { PrismaModule } from '@modules/prisma/prisma.module';
@@ -27,11 +21,4 @@ import { VerificationCodeModule } from '@modules/verificationCode/verificationCo
     }),
   ],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(BasicAuth).forRoutes({
-      path: '*',
-      method: RequestMethod.ALL,
-    });
-  }
-}
+export class AppModule {}
