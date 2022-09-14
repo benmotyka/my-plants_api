3c09f535630abf6645981c4fa1215691fb36e82c
ben <benmotykax@gmail.com>
2022-09-14 21:51:12 +0200

    completely removed verification code module and user model

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,44 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { User } from '.prisma/client';
 
 import { PrismaService } from '@modules/prisma/prisma.service';
-import { CreateUser } from '@modules/user/interfaces/CreateUser';
 import { UpsertSettingsRequestDto } from './dto/UpsertSettings.request.dto';
 import { ResponseType } from '@enums/ResponseType';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
-
-  async findOneById(id: string) {
-    return await this.prisma.user.findFirst({
-      where: {
-        id,
-      },
-    });
-  }
-
-  async findOneByUsername(username: string) {
-    return await this.prisma.user.findFirst({
-      where: {
-        username: {
-          equals: username,
-          mode: 'insensitive',
-        },
-      },
-    });
-  }
-
-  async getUserSettings(user: User) {
-    return await this.prisma.userSettings.findFirst({
-      where: {
-        userId: user.id,
-      },
-      select: {
-        pushNotificationsEnabled: true,
-      },
-    });
-  }
 
   async upsertSettings(settings: UpsertSettingsRequestDto, deviceId: string) {
     const user = await this.prisma.user.findFirst({
@@ -47,39 +15,28 @@ export class UserService {
       },
     });
 
-    await this.prisma.userSettings.upsert({
-      where: {
-        userId: user.id,
-      },
-      update: {
-        pushNotificationsEnabled: settings.pushNotificationsEnabled,
-      },
-      create: {
-        pushNotificationsEnabled: settings.pushNotificationsEnabled,
-        user: {
-          connectOrCreate: {
+    if (user) {
+      await this.prisma.userSettings.update({
+        where: {
+          userId: user.id,
+        },
+        data: {
+          pushNotificationsEnabled: settings.pushNotificationsEnabled,
+        },
+      });
+    } else {
+      await this.prisma.userSettings.create({
+        data: {
+          pushNotificationsEnabled: settings.pushNotificationsEnabled,
+          user: {
             create: {
-              deviceId,
-            },
-            where: {
               deviceId,
             },
           },
         },
-      },
-    });
+      });
+    }
 
     return ResponseType.SUCCESS;
-  }
-
-  async updatePassword(password: string, user: User) {
-    await this.prisma.user.update({
-      where: {
-        id: user.id,
-      },
-      data: {
-        password,
-      },
-    });
   }
 }
