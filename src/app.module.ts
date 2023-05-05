ba20a1734a7c9b5db0131e16f2e0faf69aa68ad5
Ben <benmotykax@gmail.com>
2023-05-05 22:46:28 +0200

    remove useless files and packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,4 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  RequestMethod,
-  NestModule,
-} from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { PlantModule } from '@modules/plant/plant.module';
@@ -11,7 +6,6 @@ import { PrismaModule } from '@modules/prisma/prisma.module';
 import { UserModule } from '@modules/user/user.module';
 import { WateringModule } from '@modules/watering/watering.module';
 import { InfoModule } from '@modules/info/info.module';
-import { BasicAuth } from '@middleware/basic-auth';
 import { LoggerMiddleware } from '@middleware/logger';
 import { UtilModule } from '@modules/util/util.module';
 // import { WinstonModule } from 'nest-winston';
@@ -36,8 +30,5 @@ import { UtilModule } from '@modules/util/util.module';
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(LoggerMiddleware).forRoutes('*');
-    // .apply(BasicAuth)
-    // .exclude({ path: 'info/patch-notes/(.*)', method: RequestMethod.ALL })
-    // .forRoutes('*');
   }
 }
